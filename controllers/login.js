65ece3d2c89ef49c80d641ab156da44a22d0aa99
NvmeSensor <priyannxhuu@gmail.com>
2024-10-21 20:15:04 +0530

    .

--- controllers/login.js
+++ controllers/login.js
@@ -33,7 +33,7 @@ const login = async (req, res) => {
     res.cookie("token", token,{
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 604800000,
     }).send();
 
