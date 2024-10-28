6bfd38551c1def952eebc19bed2a5a38c44cd4f1
Priyanshu Chahar <priyannxhuu@gmail.com>
2024-10-28 16:21:31 +0530

    .

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -73,7 +73,8 @@ export const login = async (req, res) => {
       secure: true,
       sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
-      partitioned: true
+      partitioned: true,
+      path: "/",
     });
 
     res.status(200).json({ message: "User logged in successfully" });
