048f25724388221bd180c6d513a312816bea17e5
Priyanshu Chahar <priyannxhuu@gmail.com>
2024-10-28 15:42:52 +0530

    .

--- index.js
+++ index.js
@@ -17,8 +17,6 @@ const app = express();
 
 app.use(cors({
   origin: process.env.CLIENT_URL,
-  methods: "GET, POST, PUT, DELETE",
-  allowedHeaders: "Content-Type, Authorization",
   credentials: true
 }));
 
